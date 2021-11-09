e1005676a4662d034f9ee3d2ab65f33bb9c1a162
Paulo Vitor Carvalho Santos <p.vitor_8@hotmail.com>
2021-11-09 16:07:57 -0300

    implementação de recursos em massa para o sistema de rotas

--- routes/web.php
+++ routes/web.php
@@ -27,34 +27,60 @@ options
 //     return view('welcome');
 //     return "Olá seja bem vindo ao curso!";
 // });
-
 // Route::get('/sobre-nos', function(){
 //     return 'Sobre-nós';
 // });
-
 // Route::get('/contato', function(){
 //     return 'Contato';
 // });
 
-
-Route::get('/', 'App\Http\Controllers\PrincipalController@principal');
-Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos');
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+/* rotas com pametros opcionais e tipagagem nas variaveis  */
 
 // Route::get('/contato/{nome}/{sexo?}/{idade?}', function(string $nome, string $sexo = 'indefinido', int $idade = 0){
 //           echo "Estamos aqui: {$nome} - {$sexo} - {$idade}";
 // });
 
-Route::get('
-          /contato/{nome}/{categoria_id}', 
-          function(
-                    string $nome = 'Desconhecido', 
-                    int $categoria_id = 1 // 1 = informação
-                    ){
-          echo "Estamos aqui: {$nome} - {$categoria_id}";
-          }
-)->where('nome', '[A-Za-z]+')
-->where('categoria_id', '[0-9]+');
+/* Tratativas de parametros com expressão regular*/
+
+// Route::get('
+//           /contato/{nome}/{categoria_id}', 
+//           function(
+//                     string $nome = 'Desconhecido', 
+//                     int $categoria_id = 1 // 1 = informação
+//                     ){
+//           echo "Estamos aqui: {$nome} - {$categoria_id}";
+//           }
+// )->where('nome', '[A-Za-z]+')
+// ->where('categoria_id', '[0-9]+');
+
+/* Redirecionamente de rotas */
+
+// Route::get('/rota1', function(){
+//           echo 'Rota 1';
+// })->name('site.rota1');
+// Route::get('/rota2', function(){
+//           //echo 'Rota 2';
+//           return redirect()->route('site.rota1');
+// })->name('site.rota2');
+//Route::redirect('/rota2', '/rota1'); //redirecionamento de routas -> ao acessar a rota2 automaticamente será redirecionado para rota1
+
+/* Rota de contigência: caso usuário acessa uma determinada rota inexistente, ele cairá nessa rota de fallback*/
+Route::fallback(function(){
+          echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+});
+
+Route::get('/', 'App\Http\Controllers\PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+
+Route::prefix('/app')->group(function(){
+          Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+          Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+          Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+});
+
 
 
 
