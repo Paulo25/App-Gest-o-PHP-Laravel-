f2b76af4695a5fee733662d56af59ffb870d05bc
Paulo Vitor Carvalho Santos <p.vitor_8@hotmail.com>
2021-12-23 01:00:33 -0300

    estudante recursos de criação, atualização e  remoção com eloquent

--- app/Models/SiteContato.php
+++ app/Models/SiteContato.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-//conversão que o laravel faz para identificar um model no banco de dados
+//conversão que o laravel faz para identificar a tabela no banco de dados correspondente ao model
 //Site_Contato -> site_contato -> site_contatos
 
 class SiteContato extends Model
