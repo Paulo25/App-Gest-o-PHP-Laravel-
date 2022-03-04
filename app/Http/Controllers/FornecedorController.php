0ee0951b46b29f82b82c4a190e6369d9673c22b2
Paulo_Vitor_Carvalho_Santos <paulo.santos.ext@infracommerce.com.br>
2022-03-04 00:50:21 -0300

    criando views fornecedor

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -10,4 +10,12 @@ class FornecedorController extends Controller
     public function index(){
         return view('app.fornecedor.index');
     }
+    
+    public function listar(){
+        return view('app.fornecedor.listar');
+    }
+
+    public function adicionar(){
+        return view('app.fornecedor.adicionar');
+    }
 }
--- resources/views/app/fornecedor/adicionar.blade.php
+++ resources/views/app/fornecedor/adicionar.blade.php
@@ -7,7 +7,7 @@
  {{-- <br/><br/><br/><br/>FORNECEDOR --}}
 
     <div class="titulo-pagina-2">
-        <p>Fornecedor</p>
+        <p>Fornecedor - Adicionar</p>
     </div>
 
     <div class="menu">
@@ -19,13 +19,12 @@
 
     <div class="informacao-pagina">
         <div style="width:30%; margin-left:auto; margin-right:auto;">
-            <form method="post" action="{{route('app.fornecedor.listar')}}">
-                @csrf
+            <form method="post" action="">
                 <input type="text" name="nome" placeholder="Nome" class="bolda-preta"/>
                 <input type="text" name="site" placeholder="Site" class="bolda-preta"/>
                 <input type="text" name="uf" placeholder="UF" class="bolda-preta"/>
                 <input type="text" name="email" placeholder="E-mail" class="bolda-preta"/>
-                <button type="submit" class="borda-preta">Pesquisar</button>
+                <button type="submit" class="borda-preta">Cadastrar</button>
             </form>
         </div>
     </div>
